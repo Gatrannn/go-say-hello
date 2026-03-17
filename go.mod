e6d4c31ec2da9a20863232efe62b31a37b08ae8c
Gatran <kitadeep1919@gmail.com>
2026-03-17 11:27:21 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Gatrannn/go-say-hello
+module github.com/Gatrannn/go-say-hello/v2
 
 go 1.25.4
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func Sayhello() string{
-	return "Hallo World"
+func Sayhello(name string) string{
+	return "Hallo World" + name
 }
